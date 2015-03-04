1974169f4ef31476f4793113a7f6a5afc96d12c9
pankajtyagi2007 <tyagi20071989@gmail.com>
2015-03-04 14:10:07 +0530

    test edit

--- eventManagement/src/com/aartek/eventManagement/Test.java
+++ eventManagement/src/com/aartek/eventManagement/Test.java
@@ -5,9 +5,7 @@ public class Test
 
 	public static void main(String[] args)
 	{
-	System.out.println("hello");
-	System.out.println("love u vipul");
-	System.out.println("Luv you to :*");
+		System.out.println("hello");
 	}
 
 }
